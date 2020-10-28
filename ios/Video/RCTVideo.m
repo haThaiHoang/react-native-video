71b4feb22b3a53ee634dc953c2f3e9ed6998219c
dungnbap <71741439+dungnbap@users.noreply.github.com>
2020-10-28 14:18:00 +0700

    Fix Dimension iOS 14

--- ios/Video/RCTVideo.m
+++ ios/Video/RCTVideo.m
@@ -383,7 +383,9 @@ static int const RCTVideoUnset = -1;
       }
       
       self->_player = [AVPlayer playerWithPlayerItem:self->_playerItem];
-      [self touchPlayerLayer];
+      if(self->_filterEnabled) {
+        [self touchPlayerLayer];
+      }
       self->_player.actionAtItemEnd = AVPlayerActionAtItemEndNone;
       
       [self->_player addObserver:self forKeyPath:playbackRate options:0 context:nil];
